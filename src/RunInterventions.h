dfe7646594eb6287fc7c12119f7fc54624b1307b
dchodge <david_hodgson1991@hotmail.co.uk>
2021-07-21 10:50:17 +0100

    Changes to run_sample_custom

--- src/RunInterventions.h
+++ src/RunInterventions.h
@@ -14,7 +14,7 @@
 #include <boost/random.hpp>
 #include <boost/math/distributions.hpp>
 #include "ascent/Ascent.h"
-#include "manipContacts.h"
+//#include "manipContacts.h"
 
 // [[Rcpp::depends(RcppEigen)]]
 // [[Rcpp::plugins("cpp14")]]
